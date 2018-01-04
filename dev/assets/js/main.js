35d0c0d50b0df63cc6fba0f5944106382f501389
Amanda McIntosh <am.mcintosh1@gmail.com>
2018-01-04 14:41:48 -0500

    New website version, including seperating the 'works' and the 'resume' pages out from the homepage

---
+++ dev/assets/js/main.js
@@ -0,0 +1,93 @@
+$(document).ready(function() {
+  addBackgroundToPieces();
+
+  $('.pieces .piece a').magnificPopup({
+    type:'image',
+    gallery: {
+      enabled: true
+    },
+  });
+  $('.company').each(function(){
+    let el = $(this)
+    el.addClass('condensed')
+  })
+
+  projectControls();
+});
+
+
+function addBackgroundToPieces(){
+  // TODO: Add small image instead of full-sized image to improve load time.
+
+  let pieces =  $('.pieces .piece');
+
+  pieces.each(function(i, e){
+    var piece = $(e),
+        link = piece.children('a').attr('href');
+
+    piece.attr('style',`background:url(${link}); background-size: cover; background-repeat: no-repeat; background-position: center center;`)
+
+    let img = piece.find('img')
+
+    img.remove()
+  })
+}
+
+
+function projectControls(){
+  $('.company.condensed').on('mouseenter', function(e){
+  let mouseOriginX = e.pageX,
+      mouseOriginY = e.pageY
+
+  // Simple interactive move effect
+  //
+  $(this).on('mousemove', function (e) {
+    let mousePositionX = e.pageX,
+        mousePositionY = e.pageY,
+        newMarginX = -50 + (mouseOriginX - mousePositionX)/2 + '%',
+        newMarginY = (mouseOriginY - mousePositionY)/4 + 'px'
+
+    $(this).children('.project:first-of-type').css({'margin-left':newMarginX, 'margin-top':newMarginY})
+    })
+  })
+
+  $('.company.condensed').on('mouseleave', function(e){
+    let el = $(this)
+      console.log('out')
+      setTimeout(function(){
+        if(!el.hasClass('expanded')){
+        el.children('.project:first-of-type').css({'margin-left':'-50%', 'margin-top':0})
+        }
+      }, 1000)
+
+  })
+
+  $('.company.condensed').on('click', function(){
+  $(this).off();
+  $(this).addClass('expanded').removeClass('condensed').children('.project:first-of-type').css({'margin-left':'0', 'margin-top':0, 'min-height':window.innerHeight})
+    $('.pieces').addClass('hidden')
+    $('.company.condensed').addClass('hidden')
+
+    $(this).append('<div class="close">close</div>').on('click', function(){
+      $(this).removeClass('expanded').addClass('condensed')
+      $(this).children('.project:first-of-type').css({'margin-left':'-50%', 'margin-top':0, 'height':'auto', 'min-height':'auto'})
+      projectControls();
+      $('.company.condensed').removeClass('hidden')
+      $('.pieces').removeClass('hidden')
+      $('.close').remove()
+    })
+  })
+}
+
+
+//  var dropdown = $(this).next('.dropdown'),
+//      hovering = 0
+//  dropdown.show()
+//
+//  setInterval(function(){ hovering++; console.log(hovering) }, 1000);
+//
+//  $(this).on('mouseleave', function(){
+//    setTimeout(function(){ dropdown.hide() }, 1000)
+//  })
+//
+//})
---
+++ gruntfile.js
@@ -0,0 +1,84 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'dev/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.js'
+      }
+    },
+    assemble: {
+      options: {
+        assets: 'assets',
+        partials: ['dev/template/partials/**/*.hbs'],
+        data: 'dev/template/data/**/*',
+        layoutdir: 'dev/template/layout/',
+        layout: ['default.hbs'],
+        flatten: true
+      },
+      site: {
+        src: ['dev/template/*.hbs'],
+        dest: 'dev/site/'
+      }
+    },
+    cssmin: {
+      options: {
+      shorthandCompacting: false,
+      roundingPrecision: -1
+      },
+      combine: {
+        files: {
+          'build/css/style.min.css': ['dev/assets/css/*']
+        }
+      }
+    },
+    copy: {
+      main:{
+        files: [
+          {expand: true, flatten: true, src: './dev/site/*', dest:'./build/'},
+          {
+            expand: true,
+            cwd:'./dev/assets/',
+            src: ['**', '!**/css/**'],
+            dest: 'build/'}
+        ],
+      },
+    },
+    clean: {
+      build: ['./build']
+    },
+    watch: {
+      options: {
+        livereload: true,
+      },
+      build: {
+        files: ['dev/**/*'],
+        tasks: ['assemble', 'clean', 'copy', 'cssmin']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.loadNpmTasks('grunt-serve');
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-assemble');
+
+  // Default Task Overview:
+  // (1) Builds site
+  // (2) Cleans up previous build folder
+  // (3)copies built site into build
+
+  grunt.registerTask('default', ['assemble', 'clean', 'copy', 'cssmin', 'watch'])
+}
